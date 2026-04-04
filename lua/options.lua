3477f0f0fec5fab09a5d1d880d54b088961505b9
omar-abdelgawad <omar.abdelgawad@ejust.edu.eg>
2026-04-04 09:59:13 +0200

    Disable folding

--- lua/options.lua
+++ lua/options.lua
@@ -3,5 +3,6 @@ require "nvchad.options"
 -- add yours here!
 vim.opt.relativenumber = true
 require("configs.autocmds")
+vim.opt.foldenable = false
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
